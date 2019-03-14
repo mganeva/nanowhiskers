50beb2e978f808f08e0c1f6ffc4bf038082b4397
Marina Ganeva <m.ganeva@fz-juelich.de>
2019-03-14 07:29:18 +0100

    plot GISANS for SDD 1.3m for S3278

---
+++ kws3/kws3-gisans1300mm.py
@@ -0,0 +1,169 @@
+import os
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import bornagain as ba
+from os.path import join
+from ruamel.yaml import YAML
+
+
+sensitivity_matrix = "/datadisk/data/KWS3/p15749/sens.sens"
+datapath = '/home/mary/build/bornagain/users/uliana/kws3/gisans1m'
+
+
+def load_data(fname):
+    rawdata = np.loadtxt(fname)
+    sens = np.loadtxt(sensitivity_matrix)
+    data = rawdata*sens
+
+    return np.rot90(data, 3)
+
+
+def plot_2d_raw(fname, zmax=100.0):
+    data = load_data(join(datapath, fname))
+    plt.imshow(data, norm=matplotlib.colors.LogNorm(0.1, zmax), aspect='auto', cmap='jet')
+    plt.title(fname)
+    plt.show()
+
+
+def plot_2d_sum(fnames, zmax=100.0):
+    result = np.zeros((256, 256))
+    for fname in fnames:
+        data = load_data(join(datapath, fname))
+        result += data
+
+    plt.imshow(result, norm=matplotlib.colors.LogNorm(0.1, zmax), aspect='auto', cmap='jet')
+    plt.show()
+
+
+def get_kws3_detector():
+    """
+    Creates and returns KWS-3 detector
+    detector dimensions: 90x90 mm
+    SDD: 9300 mm (for SANS)
+    """
+    u0 = 45.3  # in mm
+    v0 = 29.9  # in mm
+    detector = ba.RectangularDetector(256, 90.0, 256, 90.0)
+    detector.setPerpendicularToDirectBeam(1300.0, u0, v0)
+    return detector
+
+
+def get_simulation():
+    """
+    Returns a GISAXS simulation with beam and detector defined
+    wavelength 12.8 angstrom
+    incident angle 0.0 degrees (transmission)
+    """
+    simulation = ba.GISASSimulation()
+    simulation.setBeamParameters(12.8*ba.angstrom, 0.56*ba.deg, 0.0*ba.deg)
+    simulation.setDetector(get_kws3_detector())
+    simulation.setDetectorResolutionFunction(ba.ResolutionFunction2DGaussian(5.0, 5.0))
+    simulation.setBeamIntensity(1000)
+    distr_1 = ba.DistributionGaussian(1.28*ba.nm, 0.1)
+    simulation.addParameterDistribution("*/Beam/Wavelength", distr_1, 50, 2.0, ba.RealLimits.positive())
+    simulation.getOptions().setIncludeSpecular(True)
+    return simulation
+
+
+def get_rotation_angle(fname):
+    yaml = YAML()
+    with open(join(datapath, fname.format('yaml'))) as f:
+        metadata = yaml.load(f)
+        for d in metadata['measurement']['devices']:
+            if d['name'] == 'sam_rot':
+                return d['value']
+
+
+def plot_s3278_2d():
+    fname = "0006543{r1}_000{r2}_p15749_S3278-GiSANS-0.6dgr-rot-scan-m15-15dgr_HRD_standard.{ext}"
+    runs = [(0, 1), (1, 1), (2, 2), (3, 3), (4, 4), (5, 1), (6, 2), (7, 3), (8, 0)]
+    simulation = get_simulation()
+    result = simulation.result()
+    axes_limits = ba.get_axes_limits(result, ba.AxesUnits.QSPACE)
+
+    plt.style.use('seaborn-talk')
+    plt.suptitle("Sample S3278 SDD=1300 mm", fontsize=16)
+    for i in runs:
+        data = load_data(join(datapath, fname.format(r1=i[0], r2=i[1], ext='det')))
+        omega = get_rotation_angle(join(datapath, fname.format(r1=i[0], r2=i[1], ext='yaml')))
+        ax = plt.subplot(3, 3, i[0]+1)
+        plt.subplots_adjust(wspace=0.2, hspace=0.3)
+        im = plt.imshow(data,
+                        norm=matplotlib.colors.LogNorm(0.1, 1000),
+                        extent=axes_limits,
+                        aspect='auto', cmap='jet')
+
+        cb = plt.colorbar(im)
+        # plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+        plt.xlabel(r'$Q_y$ (nm$^{-1}$)')
+        plt.ylabel(r'$Q_z$ (nm$^{-1}$)')
+        plt.title(r"Run 0006543{n}, $\omega={om}^\circ$".format(n=i[0], om=omega))
+
+    plt.show()
+
+
+def plot_s3278_qz():
+    fname = "0006543{r1}_000{r2}_p15749_S3278-GiSANS-0.6dgr-rot-scan-m15-15dgr_HRD_standard.{ext}"
+    runs = [(0, 1), (1, 1), (2, 2), (3, 3), (4, 4), (5, 1), (6, 2), (7, 3), (8, 0)]
+    simulation = get_simulation()
+    result = simulation.result()
+
+    plt.style.use('seaborn-talk')
+    plt.suptitle(r"Sample S3278 SDD=1300 mm, slice along $\alpha_f$", fontsize=16)
+    for i in runs:
+        data = load_data(join(datapath, fname.format(r1=i[0], r2=i[1], ext='det')))
+        omega = get_rotation_angle(join(datapath, fname.format(r1=i[0], r2=i[1], ext='yaml')))
+        hist = result.histogram2d(ba.AxesUnits.DEGREES)
+        hist.setContent(data)
+        ax = plt.subplot(3, 3, i[0]+1)
+        plt.subplots_adjust(wspace=0.2, hspace=0.3)
+        zslice = hist.projectionY(0.0)
+        plt.semilogy(zslice.getBinCenters(), zslice.getBinValues(), color='k', marker='.', markersize=5, linestyle='None')
+        plt.axvline(x=0.56, color='r', linestyle='--')
+        ycoord = 12000
+        if i[0] == 8:
+            ycoord=5000
+        plt.annotate(r'$\alpha_i=0.56^{\circ}$', xy=(0.56, ycoord), xytext=(0.9, 2000),
+                     fontsize=14, arrowprops=dict(facecolor='red', shrink=0.0, width=0.5, headwidth=4))
+        plt.xlabel(r'$\alpha_f$ ($^{\circ}$)')
+        plt.title(r"Run 0006543{n}, $\omega={om}^\circ$".format(n=i[0], om=omega))
+
+    plt.show()
+
+
+def plot_s3278_2d_roi():
+    fname = "0006543{r1}_000{r2}_p15749_S3278-GiSANS-0.6dgr-rot-scan-m15-15dgr_HRD_standard.{ext}"
+    runs = [(0, 1), (1, 1), (2, 2), (3, 3), (4, 4), (5, 1), (6, 2), (7, 3), (8, 0)]
+    simulation = get_simulation()
+    result = simulation.result()
+    axes_limits = [-0.05, 0.05, 0.06, 0.205]
+
+    plt.style.use('seaborn-talk')
+    plt.suptitle("Sample S3278 SDD=1300 mm", fontsize=16)
+    for i in runs:
+        data = load_data(join(datapath, fname.format(r1=i[0], r2=i[1], ext='det')))
+        omega = get_rotation_angle(join(datapath, fname.format(r1=i[0], r2=i[1], ext='yaml')))
+        hist = result.histogram2d(ba.AxesUnits.QSPACE)
+        hist.setContent(data)
+        h = hist.crop(-0.05, 0.06, 0.05, 0.205)
+        ax = plt.subplot(3, 3, i[0]+1)
+        plt.subplots_adjust(wspace=0.2, hspace=0.3)
+        im = plt.imshow(h.array(),
+                        norm=matplotlib.colors.LogNorm(0.1, 500),
+                        extent=axes_limits,
+                        aspect='auto', cmap='jet')
+
+        cb = plt.colorbar(im)
+        # plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+        plt.xlabel(r'$Q_y$ (nm$^{-1}$)')
+        plt.ylabel(r'$Q_z$ (nm$^{-1}$)')
+        plt.title(r"Run 0006543{n}, $\omega={om}^\circ$".format(n=i[0], om=omega))
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_s3278_2d()
+    plot_s3278_qz()
+    plot_s3278_2d_roi()
